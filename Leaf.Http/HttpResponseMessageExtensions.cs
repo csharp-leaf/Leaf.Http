c31ff63a00272bafb6f446d6ac5768a92a64d3d1
Grand Silence <mixtape774@gmail.com>
2019-01-31 21:56:00 +0300

    ToStringAsync() for HttpResponseMessage

---
+++ Leaf.Http/HttpResponseMessageExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+
+namespace Leaf.Http
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static string ToStringSync(this HttpResponseMessage self, bool disposeAfter = true)
+        {
+            var task = self.Content.ReadAsStringAsync();
+            task.ConfigureAwait(false);
+
+            var result = task.Result;
+
+            if (disposeAfter)
+                self.Dispose();
+
+            return result;
+        }
+    }
+}
